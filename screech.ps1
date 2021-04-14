b4846fea5d5545e886097e53e7a41b224c8b4a2a
tgmars <tmarsonearth@gmail.com>
2021-04-14 22:50:09 +1000

    Update .gitignore and do some basic logging

--- screech.ps1
+++ screech.ps1
@@ -10,11 +10,18 @@ oo     .d8P '88b    ooo   888  '88b.   888       o  888       o '88b    ooo   88
 
 Write-Host "Welcome to..."
 Write-Host -ForegroundColor Green $Header
-Write-Host "The Host Recorder`n"
+Write-Host "The host recorder`n"
 
 Write-Host "[+] Executing 'vagrant up', this may take a while on first run while we fetch Vagrant boxes for Kali & Server 2016."
+Write-Host "[+] Progress is stored in ./logs/vagrant.log"
+
+if( (Test-Path "./logs/vagrant.log") -eq $false ){
+    Out-File -FilePath "./logs/vagrant.log"
+}
+
+Write-Output "$('[{0:HH:mm}]' -f (Get-Date)) Vagrant" | Out-File -FilePath "./logs/vagrant.log" -Append
+
 Set-Location .\vagrant
-$VagrantOutput = vagrant up
+vagrant up | Out-File -FilePath "../logs/vagrant.log" -Append
 Set-Location ..
 
-Write-Host $VagrantOutput
